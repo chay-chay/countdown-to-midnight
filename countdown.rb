26f9e029d6fc9f346f588a59533b8e703c0280f1
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-10-25 13:54:21 -0400

    testing

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
 #write your code here
 
-def countdown
+# def countdown(number)
+#     while number>0
+#       puts "#{number} SECOND(S)!"
+#       number-=1
+#     end 
+#       "HAPPY NEW YEAR!"
+#   end
+
+def countdown(countdown_output)
+    while countdown_output > 0
+        puts "#{countdown_output} SECOND(S)!"
+        countdown_output -= 1
+    end    
+     "HAPPY NEW YEAR!"  
 end
+
+def countdown_with_sleep(countdown_output)
+    while countdown_output > 0
+        countdown_output -= 1
+        puts "#{countdown_output} SECOND(S)!"
+        sleep(5)
+    end    
+     "HAPPY NEW YEAR!"  
+end
+
+#countdown_with_sleep = sleep(1.second)
